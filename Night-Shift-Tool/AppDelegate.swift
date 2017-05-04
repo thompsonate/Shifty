7594c8ef6a46c31db11d48b1f0c3106751826303
thompsonate <nate@natethompson.io>
2017-05-03 20:47:17 -0400

    Initial Commit

---
+++ Night-Shift-Tool/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Night-Shift-Tool
+//
+//  Created by Nate Thompson on 5/3/17.
+//
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
