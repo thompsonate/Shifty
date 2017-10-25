2a2aa13469cf7d7d6d28e30d21f44f2846f0a861
thompsonate <nate@natethompson.io>
2017-10-24 21:42:46 -0400

    update NSSlider subclass with more edge cases

--- Shifty/SliderView.swift
+++ Shifty/SliderView.swift
@@ -30,16 +30,42 @@ class SliderView: NSView {
 }
 
 
-class NSSliderWithScroll: NSSlider {
+class ScrollableSlider: NSSlider {
     override func scrollWheel(with event: NSEvent) {
+        guard self.isEnabled else { return }
+        
         let range = Float(self.maxValue - self.minValue)
-        var delta = Float(event.deltaY - event.deltaX)
+        var delta = Float(0)
+        
+        //Allow horizontal scrolling on horizontal and circular sliders
+        if self.isVertical && self.sliderType == .linear {
+            delta = Float(event.deltaY)
+        } else if self.userInterfaceLayoutDirection == .rightToLeft {
+            delta = Float(event.deltaY + event.deltaX)
+        } else {
+            delta = Float(event.deltaY - event.deltaX)
+        }
+        
+        //Account for natural scrolling
         if event.isDirectionInvertedFromDevice {
             delta *= -1
         }
         
         let increment = range * delta / 100
-        let value = self.floatValue + increment
+        var value = self.floatValue + increment
+        
+        //Wrap around if slider is circular
+        if self.sliderType == .circular {
+            let minValue = Float(self.minValue)
+            let maxValue = Float(self.maxValue)
+            
+            if value < minValue {
+                value = maxValue - fabs(increment)
+            }
+            if value > maxValue {
+                value = minValue + fabs(increment)
+            }
+        }
         
         self.floatValue = value
         self.sendAction(self.action, to: self.target)
