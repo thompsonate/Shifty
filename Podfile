ef9b876d9ad725d0e79b93eb7f6668cc196b1a52
thompsonate <nate@natethompson.io>
2017-11-10 00:38:27 -0500

    add MASPreferences to Podfile

--- Podfile
+++ Podfile
@@ -3,6 +3,7 @@ platform :osx, '10.12'
 target 'Shifty' do
   use_frameworks!
   pod 'Sparkle'
+  pod 'MASPreferences'
 end
 
 target 'ShiftyHelper' do
--- Pods/Target Support Files/Pods-Shifty/Pods-Shifty-frameworks.sh
+++ Pods/Target Support Files/Pods-Shifty/Pods-Shifty-frameworks.sh
@@ -89,9 +89,11 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/MASPreferences/MASPreferences.framework"
   install_framework "${PODS_ROOT}/Sparkle/Sparkle.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/MASPreferences/MASPreferences.framework"
   install_framework "${PODS_ROOT}/Sparkle/Sparkle.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
