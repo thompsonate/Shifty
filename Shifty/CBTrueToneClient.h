176786609655cd7390a2c3a134c8633f39962b85
Nate Thompson <nate@natethompson.io>
2018-09-04 22:09:28 -0400

    hook in to CBTrueToneClient to control True Tone

---
+++ Shifty/CBTrueToneClient.h
@@ -0,0 +1,14 @@
+//
+//  CBTrueToneClient.h
+//  Shifty
+//
+//  Created by Nate Thompson on 9/4/18.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CBTrueToneClient : NSObject
+- (BOOL)available;
+- (BOOL)enabled;
+- (BOOL)setEnabled:(BOOL)arg1;
+@end
---
+++ Shifty/TrueToneClient+Shifty.swift
@@ -0,0 +1,25 @@
+//
+//  TrueToneClient+Shifty.swift
+//  Shifty
+//
+//  Created by Nate Thompson on 9/4/18.
+//
+
+import Foundation
+
+extension CBTrueToneClient {
+    static var shared = CBTrueToneClient()
+    
+    var isTrueToneSupported: Bool {
+        return CBTrueToneClient.shared.available()
+    }
+    
+    var isTrueToneEnabled: Bool {
+        get {
+            return CBTrueToneClient.shared.enabled()
+        }
+        set {
+            CBTrueToneClient.shared.setEnabled(newValue)
+        }
+    }
+}
