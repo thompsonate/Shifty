35963db10f694cff566c5285d7bddecb87a2c7a1
Nate Thompson <nate@natethompson.io>
2018-09-27 23:43:10 -0400

    add Twitter link to about page

--- Shifty/Event.swift
+++ Shifty/Event.swift
@@ -35,6 +35,7 @@ enum Event {
     case shortcuts(toggleNightShift: Bool, increaseColorTemp: Bool, decreaseColorTemp: Bool, disableApp: Bool, disableDomain: Bool, disableSubdomain: Bool, disableHour: Bool, disableCustom: Bool)
     case websiteButtonClicked
     case feedbackButtonClicked
+    case twitterButtonClicked
     case translateButtonClicked
     case donateButtonClicked
     case checkForUpdatesClicked
@@ -75,6 +76,7 @@ extension Event {
         case .shortcuts: return "Shortcuts"
         case .websiteButtonClicked: return "Website button clicked"
         case .feedbackButtonClicked: return "Feedback button clicked"
+        case .twitterButtonClicked: return "Twitter button clicked"
         case .translateButtonClicked: return "Translate button clicked"
         case .donateButtonClicked: return "Donate button clicked"
         case .checkForUpdatesClicked: return "Check for updates button clicked"
--- Shifty/PrefAboutViewController.swift
+++ Shifty/PrefAboutViewController.swift
@@ -68,7 +68,13 @@ class PrefAboutViewController: NSViewController, MASPreferencesViewController {
         NSWorkspace.shared.open(url)
         Event.feedbackButtonClicked.record()
     }
-
+    
+    @IBAction func twitterButtonClicked(_ sender: Any) {
+        guard let url = URL(string: "https://natethompson.io/twitter") else { return }
+        NSWorkspace.shared.open(url)
+        Event.twitterButtonClicked.record()
+    }
+    
     @IBAction func translateButtonClicked(_ sender: NSButton) {
         guard let url = URL(string: "https://shifty.natethompson.io/translate") else { return }
         NSWorkspace.shared.open(url)
