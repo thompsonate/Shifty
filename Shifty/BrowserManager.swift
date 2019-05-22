b57b5932cbee4f4306e07b0367fa17c2737c005f
Luming Yin <vista980622@gmail.com>
2019-05-21 22:43:56 -0700

    Replace windows.count check with windows.firstObject.

--- Shifty/BrowserManager.swift
+++ Shifty/BrowserManager.swift
@@ -242,7 +242,7 @@ enum BrowserManager {
             logw("Error: Could not get url, app already closed")
             return nil
         }
-        guard let windows = browser.windows?(), windows.count > 0, let window = windows[0] as? Window else {
+        guard let windows = browser.windows?(), let window = windows.firstObject as? Window else {
             logw("Error: Could not get url, there are no windows")
             return nil
         }
